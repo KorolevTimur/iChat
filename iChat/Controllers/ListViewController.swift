6557539b9625a25d59ab6ac3f3ef42a2987c42b3
Timur Korolev <timurkorolev89@gmail.com>
2020-04-22 18:49:34 +0300

    Main Tab Bar Controller

---
+++ iChat/Controllers/ListViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ListViewController.swift
+//  iChat
+//
+//  Created by admin on 4/22/20.
+//  Copyright © 2020 Timur Korolev. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct ListVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let listVC = ListViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ListVCProvider.ContainerView>) -> ListViewController {
+            return listVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ListVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ListVCProvider.ContainerView>) {
+            
+        }
+    }
+}
---
+++ iChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleViewController.swift
+//  iChat
+//
+//  Created by admin on 4/22/20.
+//  Copyright © 2020 Timur Korolev. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
