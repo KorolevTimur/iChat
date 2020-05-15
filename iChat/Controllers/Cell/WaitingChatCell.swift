a7e396210d4177fc21eabcd07f715219965e4b8a
Timur Korolev <timurkorolev89@gmail.com>
2020-05-15 17:53:16 +0300

    Waiting Chat Cell

---
+++ iChat/Controllers/Cell/WaitingChatCell.swift
@@ -0,0 +1,43 @@
+//
+//  WaitingChatCell.swift
+//  iChat
+//
+//  Created by admin on 5/15/20.
+//  Copyright © 2020 Timur Korolev. All rights reserved.
+//
+
+import UIKit
+
+class WaitingChatCell: UICollectionViewCell, SelfConfiguringCell {
+    static var reuseId: String = "WaitingChatCell"
+    
+    let friendImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
+        setupConstraints() 
+    }
+    
+    func configure(with value: MChat) {
+        friendImageView.image = UIImage(named: value.userImageString)
+    }
+    
+    private func setupConstraints() {
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(friendImageView)
+        
+        NSLayoutConstraint.activate([
+            friendImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            friendImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            friendImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ iChat/Protocols/SelfConfiguringCell.swift
@@ -0,0 +1,14 @@
+//
+//  SelfConfiguringCell.swift
+//  iChat
+//
+//  Created by admin on 5/15/20.
+//  Copyright © 2020 Timur Korolev. All rights reserved.
+//
+
+import Foundation
+
+protocol SelfConfiguringCell {
+    static var reuseId: String { get }
+    func configure(with value: MChat)
+}
